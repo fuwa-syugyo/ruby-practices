f1eb2cbb7cd703c9b656de9af9838550f9ce5415
fuwa <fuwafuwa41.glr@gmail.com>
2021-07-16 08:44:30 +0900

    delete unnecessary files and modify ls

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,69 +1,70 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'find'
 require 'optparse'
 require 'fileutils'
 require 'etc'
 
-options = ARGV.getopts('arl')
+OUTPUT_COLUMN_SIZE = 3
+MODE_MAP = {
+  '01' => 'p',
+  '02' => 'c',
+  '04' => 'd',
+  '06' => 'b',
+  '10' => '.',
+  '12' => 'l',
+  '14' => 's',
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-x-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
 
-file_all = []
-Dir.foreach('.') do |file|
-  next if ['.', '..'].include?(file)
+def main
+  file_all = []
+  options = ARGV.getopts('arl')
+  Dir.foreach('.') do |file|
+    next if ['.', '..'].include?(file)
 
-  file_all << file
-end
-file_all_sort = file_all.sort!
-
-def make_file_type(mode_alphabet_array)
-  mode_alphabet_array.each do |n|
-    case n
-    when '01' then mode_alphabet_array[0] = 'p'
-    when '02' then mode_alphabet_array[0] = 'c'
-    when '04' then mode_alphabet_array[0] = 'd'
-    when '06' then mode_alphabet_array[0] = 'b'
-    when '10' then mode_alphabet_array[0] = '.'
-    when '12' then mode_alphabet_array[0] = 'l'
-    when '14' then mode_alphabet_array[0] = 's'
-    end
+    file_all << file
   end
-end
+  file_all_sort = file_all.sort!
 
-def make_permission(mode_alphabet_array)
-  permission_array = [mode_alphabet_array.first]
-  mode_alphabet_array.each do |n|
-    case n
-    when '0' then permission_array << '---'
-    when '1' then permission_array << '--x'
-    when '2' then permission_array << '-x-'
-    when '3' then permission_array << '-wx'
-    when '4' then permission_array << 'r--'
-    when '5' then permission_array << 'r-x'
-    when '6' then permission_array << 'rw-'
-    when '7' then permission_array << 'rwx'
-    end
-  end
-  permission_array.join
-end
+  file_all_sort = exclude_hidden_files(file_all_sort) if options['a'] == false
 
-def permission_alphabet(stat)
-  mode_alphabet = stat.mode.to_s(8)
-  if stat.ftype == 'fifo' || stat.ftype == 'characterSpecial' || stat.ftype == 'directory' || stat.ftype == 'blockSpecial'
-    mode_alphabet = "0#{stat.mode.to_s(8)}" # ファイルタイプが一桁のとき頭に0を入れる
+  option_r(file_all_sort) if options['r'] == true
+
+  if options['l'] == true
+    option_l(file_all_sort)
   else
-    mode_alphabet
+    same_lines_columns(file_all_sort)
   end
-
-  mode_alphabet_array = mode_alphabet.chars
-  mode_alphabet_array[0, 3] = mode_alphabet_array[0..1].join
-  make_file_type(mode_alphabet_array)
-  make_permission(mode_alphabet_array)
 end
 
-def option_without_a(file_all_sort)
+def exclude_hidden_files(file_all_sort)
   hidden_file = file_all_sort.grep(/^\./)
   file_all_sort - hidden_file
+end
+
+def option_r(file_all_sort)
+  file_all_sort.reverse!
+end
+
+def same_lines_columns(file_all_sort)
+  if (file_all_sort.size % OUTPUT_COLUMN_SIZE).zero?
+    column_size = file_all_sort.size / OUTPUT_COLUMN_SIZE
+  else
+    column_size = file_all_sort.size / OUTPUT_COLUMN_SIZE + 1 # 配列の行と列の個数を揃える
+    (column_size * OUTPUT_COLUMN_SIZE - file_all_sort.size).times do
+      file_all_sort << ' '
+    end
+  end
+
+  puts(file_all_sort.each_slice(column_size).to_a.transpose.map { |e| e.join '  ' })
 end
 
 def option_l(file_all_sort)
@@ -74,31 +75,25 @@ def option_l(file_all_sort)
   end
 end
 
-def puts_3column(file_all_sort)
-  column = 3
-
-  if (file_all_sort.size % column).zero?
-    column_size = file_all_sort.size / column
+def permission_alphabet(stat)
+  mode_alphabet = stat.mode.to_s(8)
+  if stat.ftype == 'fifo' || stat.ftype == 'characterSpecial' || stat.ftype == 'directory' || stat.ftype == 'blockSpecial'
+    mode_alphabet = "0#{stat.mode.to_s(8)}"
   else
-    column_size = file_all_sort.size / column + 1 # 配列の行と列の個数を揃える
-    (column_size * column - file_all_sort.size).times do
-      file_all_sort << ' '
-    end
+    mode_alphabet
   end
 
-  puts(file_all_sort.each_slice(column_size).to_a.transpose.map { |e| e.join '  ' })
+  mode_alphabet_array = mode_alphabet.chars
+  mode_alphabet_array[0, 3] = mode_alphabet_array[0..1].join
+  make_permission(mode_alphabet_array)
 end
 
-file_all_sort = option_without_a(file_all_sort) if options['a'] == false
-
-def option_r(file_all_sort)
-  file_all_sort.reverse!
+def make_permission(mode_alphabet_array)
+  permission_array = []
+  mode_alphabet_array.each do |n|
+    permission_array << MODE_MAP[n]
+  end
+  permission_array.join
 end
 
-option_r(file_all_sort) if options['r'] == true
-
-if options['l'] == true
-  option_l(file_all_sort)
-else
-  puts_3column(file_all_sort)
-end
+main
