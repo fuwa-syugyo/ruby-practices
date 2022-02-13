05a5554462b257171172f6a0fa97449e0b7b1ca3
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-13 16:31:01 +0900

    テストをサンプル通りに

--- 07.bowling_object/game_test.rb
+++ 07.bowling_object/game_test.rb
@@ -4,25 +4,33 @@ require_relative 'game.rb'
 require 'minitest/autorun'
 
 class GameTest < Minitest::Test
-  def test_calc_final_frame_perfect
-    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
-    assert_equal 164, game.calc_score
-  end
-
   def test_calc_spare_or_strike
     game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5')
     assert_equal 139, game.calc_score
   end
 
-#   def test_calc_X_or_10
-#     assert_equal 107, calc_score('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
-#   end
+  def test_calc_final_frame_perfect
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X')
+    assert_equal 164, game.calc_score
+  end
 
-#   def test_calc_all_zero
-#     assert_equal 0, calc_score('0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
-#   end
+  def test_calc_X_or_10
+    game = Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4')
+    assert_equal 107, game.calc_score
+  end
 
-#   def test_calc_perfect
-#     assert_equal 300, calc_score('X,X,X,X,X,X,X,X,X,X,X,X')
-#   end
+  def test_calc_last_frame_10
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0')
+    assert_equal 134, game.calc_score
+  end
+
+  def test_calc_all_zero
+    game = Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8')
+    assert_equal 144, game.calc_score
+  end
+
+  def test_calc_perfect
+    game = Game.new('X,X,X,X,X,X,X,X,X,X,X,X')
+    assert_equal 300, game.calc_score
+  end
 end
