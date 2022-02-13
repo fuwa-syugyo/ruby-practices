bfe9ef53077ebe3e46bfab80fa73462e76d58b8d
fuwa <fuwafuwa41.glr@gmail.com>
2022-02-13 14:48:58 +0900

    attr_readerの削除＆first_shot等をインスタンス変数化

--- 07.bowling_object/frame.rb
+++ 07.bowling_object/frame.rb
@@ -3,8 +3,6 @@
 require_relative 'shot'
 
 class Frame
-  attr_reader :first_shot, :second_shot, :third_shot
-
   def initialize(first_mark, second_mark, third_mark = nil)
     @first_shot = Shot.new(first_mark)
     @second_shot = Shot.new(second_mark)
@@ -12,10 +10,10 @@ class Frame
   end
 
   def calc_frame
-    if first_shot.score == 10 || [first_shot.score, second_shot.score].sum ==10
-      [first_shot.score, second_shot.score, third_shot.score].sum
+    if @first_shot.score == 10 || [@first_shot.score, @second_shot.score].sum ==10
+      [@first_shot.score, @second_shot.score, @third_shot.score].sum
     else
-      [first_shot.score, second_shot.score].sum
+      [@first_shot.score, @second_shot.score].sum
     end
   end
 
@@ -25,7 +23,7 @@ class Frame
   end
 
   def spare?
-    [first_shot.score, second_shot.score].sum == 10 && first_shot.score != 10
+    [@first_shot.score, @second_shot.score].sum == 10 && @first_shot.score != 10
     # 次の1投の得点追加の処理もここで？
   end
 end
